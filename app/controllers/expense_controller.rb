05382697c4feb57350f425d582b05fc2f6e099c4
sarisgar28 <sarisgar28@gmail.com>
2021-03-30 12:50:06 -0400

    controllers

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -1,2 +1,33 @@
 class ExpenseController < ApplicationController
+    before_action :find_user
+    before_action :authorized
+
+
+    def index 
+        @expenses = @user.expenses
+        render json: @expenses
+    end 
+
+    def show
+        @expense = @user.expense.find_by(id: params[:id])
+        render json: @expense
+    end 
+    
+    def create
+    end 
+
+    def update 
+    end 
+    
+    def destroy
+    end 
+    
+    
+    
+    
+    private 
+
+    def find_user
+        @user = User.find_by(id: params[:user_id])
+      end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,16 +1,39 @@
 class UserController < ApplicationController
 
+    before_action :authorized, only: [:auto_login]
 
-    def index 
-      @users = User.all 
-      render json: @users
-    end 
-
-    def show
-        @user = User.find_by(id: params[:id]) 
-        render json: @user 
-    end 
-    
-    def create 
+    # REGISTER
+    def create
+      @user = User.create(user_params)
+      if @user.valid?
+        token = encode_token({user_id: @user.id})
+        render json: {user: @user, token: token}
+      else
+        render json: {error: "Invalid username or password"}
+      end
     end
+  
+    # LOGGING IN
+    def login
+      @user = User.find_by(username: params[:username])
+  
+      if @user && @user.authenticate(params[:password])
+        token = encode_token({user_id: @user.id})
+        render json: {user: @user, token: token}
+      else
+        render json: {error: "Invalid username or password"}
+      end
+    end
+  
+  
+    def auto_login
+      render json: @user
+    end
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+      end
 end
